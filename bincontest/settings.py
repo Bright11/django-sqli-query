7712b9964dd541718f5e6bacf848dc947648273f
Bright11 <chikanwazuo@gmail.com>
2023-09-10 15:47:48 +0000

    update

--- bincontest/settings.py
+++ bincontest/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     "whitenoise.middleware.WhiteNoiseMiddleware",#my code
+    # "whitenoise.middleware.WhiteNoiseMiddleware",#my code
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL="media/"
 MEDIA_ROOT=BASE_DIR/"media"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
