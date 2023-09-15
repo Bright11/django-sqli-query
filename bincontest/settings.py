b21936e9cf7d8b54ebffd77206e3ff6ca367ad7c
Bright11 <chikanwazuo@gmail.com>
2023-09-15 19:25:17 +0000

    update

--- bincontest/settings.py
+++ bincontest/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER':'root',
          'HOST':'localhost',
          'PORT':3306,
-        # 'PASSWORD':'',
+         'PASSWORD':'',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
